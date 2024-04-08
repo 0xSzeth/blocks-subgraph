ca58d9c1c116f50ed7517ebb9fedbf2280811a83
OxSzeth <0xSzeth@gmail.com>
2024-04-08 10:57:40 -0600

    Initial commit

---
+++ tests/weth-utils.ts
@@ -0,0 +1,106 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  Approval,
+  Deposit,
+  OwnershipTransferred,
+  Transfer,
+  Withdrawal
+} from "../generated/WETH/WETH"
+
+export function createApprovalEvent(
+  src: Address,
+  guy: Address,
+  wad: BigInt
+): Approval {
+  let approvalEvent = changetype<Approval>(newMockEvent())
+
+  approvalEvent.parameters = new Array()
+
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("src", ethereum.Value.fromAddress(src))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("guy", ethereum.Value.fromAddress(guy))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("wad", ethereum.Value.fromUnsignedBigInt(wad))
+  )
+
+  return approvalEvent
+}
+
+export function createDepositEvent(dst: Address, wad: BigInt): Deposit {
+  let depositEvent = changetype<Deposit>(newMockEvent())
+
+  depositEvent.parameters = new Array()
+
+  depositEvent.parameters.push(
+    new ethereum.EventParam("dst", ethereum.Value.fromAddress(dst))
+  )
+  depositEvent.parameters.push(
+    new ethereum.EventParam("wad", ethereum.Value.fromUnsignedBigInt(wad))
+  )
+
+  return depositEvent
+}
+
+export function createOwnershipTransferredEvent(
+  previousOwner: Address,
+  newOwner: Address
+): OwnershipTransferred {
+  let ownershipTransferredEvent = changetype<OwnershipTransferred>(
+    newMockEvent()
+  )
+
+  ownershipTransferredEvent.parameters = new Array()
+
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousOwner",
+      ethereum.Value.fromAddress(previousOwner)
+    )
+  )
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
+  )
+
+  return ownershipTransferredEvent
+}
+
+export function createTransferEvent(
+  src: Address,
+  dst: Address,
+  wad: BigInt
+): Transfer {
+  let transferEvent = changetype<Transfer>(newMockEvent())
+
+  transferEvent.parameters = new Array()
+
+  transferEvent.parameters.push(
+    new ethereum.EventParam("src", ethereum.Value.fromAddress(src))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("dst", ethereum.Value.fromAddress(dst))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("wad", ethereum.Value.fromUnsignedBigInt(wad))
+  )
+
+  return transferEvent
+}
+
+export function createWithdrawalEvent(src: Address, wad: BigInt): Withdrawal {
+  let withdrawalEvent = changetype<Withdrawal>(newMockEvent())
+
+  withdrawalEvent.parameters = new Array()
+
+  withdrawalEvent.parameters.push(
+    new ethereum.EventParam("src", ethereum.Value.fromAddress(src))
+  )
+  withdrawalEvent.parameters.push(
+    new ethereum.EventParam("wad", ethereum.Value.fromUnsignedBigInt(wad))
+  )
+
+  return withdrawalEvent
+}
